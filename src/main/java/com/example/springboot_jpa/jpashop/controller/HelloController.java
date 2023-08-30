da334f3288ab55981e2d7108773c1b510c043f4e
JeonB <newjeonb@gmail.com>
2023-08-31 01:17:02 +0900

    Spring JPA Day1 김영한 실전 스프링부트와 JPA 활용 1일차 h2연결, Member엔티티, Repository, 테스트 생성 트랜잭션화의 중요성, 롤백 비활성화 방법, application.yml설정 학습

--- src/main/java/com/example/springboot_jpa/jpashop/controller/HelloController.java
+++ src/main/java/com/example/springboot_jpa/jpashop/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.springboot_jpa.jpashop;
+package com.example.springboot_jpa.jpashop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
