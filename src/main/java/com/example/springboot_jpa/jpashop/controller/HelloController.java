2d722d848e8419872e5bfac4bfbc914a852a8bcd
JeonB <newjeonb@gmail.com>
2023-10-19 21:07:46 +0900

    what is this

--- src/main/java/com/example/springboot_jpa/jpashop/controller/HelloController.java
+++ src/main/java/com/example/springboot_jpa/jpashop/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.springboot_jpa.jpashop.controller;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,5 +12,13 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("/thisIsTest")
+    public String test(@Param("nono") String str,Model model){
+        String testValue = "";
+        model.addAttribute(("fuchk"), testValue );
+
+        return "amazing";
     }
 }
